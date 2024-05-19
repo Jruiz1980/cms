57d695027fc8b2c4f7622fc6822d7449d064922f
Jruiz1980 <johnathanruiz@gmail.com>
2024-05-19 05:03:11 -0500

    add doc names

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -10,10 +10,14 @@ export class DocumentListComponent {
   @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
   documents: Document[] = [
-    new Document('001', 'test1', 'a test document.', '../../assets/test001.docx', null),
-    new Document('002', 'test2', 'a test document.', '../../assets/test002.docx', null),
-    new Document('003', 'test3', 'a test document.', '../../assets/test003.docx', null),
-    new Document('004', 'test4', 'a test document.', '../../assets/test004.docx', null)
+    new Document('001', 'winter', 'a winter document.', 
+                  'https://en.islcollective.com/english-esl-worksheets/winter/131001', null),
+    new Document('002', 'spring', 'a spring document.', 
+    'https://en.islcollective.com/english-esl-worksheets/general-topic/spring/spring/15524', null),
+    new Document('003', 'summer', 'a summer document.', 
+    'https://en.islcollective.com/english-esl-worksheets/general-topic/summer/summer/8670', null),
+    new Document('004', 'fall', 'a fall document.', 
+    'https://en.islcollective.com/english-esl-worksheets/fall-foods/41528', null)
   ]
 
   onSelectedDocument(document: Document){
